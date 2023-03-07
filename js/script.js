6baeaa9795b289b8bd8d4f450852275080884600
anazerlin <rm98065@fiap.com.br>
2023-03-07 11:48:57 -0300

    script.js adicionado ao html

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//este é um comentario de linha
+
+/**
+ * 
+ * este
+ * é um comentario
+ * de bloco
+ */
+
+console.log("olá mundo")
+console.error("bom dia")
+console.warn("será?")
+console.info("vai ser...")
